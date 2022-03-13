29326399cd730565770b2346c44e67fe3b9d489f
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-03-13 19:02:24 +0000

    update read_header
    adjusted i and j loops

--- read_header.m
+++ read_header.m
@@ -13,8 +13,8 @@ headerlines = all_lines(1:(lines_header)); % truncate the data beyond the end of
 
 % counts characters in the header
 len_header = 0; 
-for i = 1:lines_header
-    len_header = len_header + strlength(headerlines(i)) +2;
+for k = 1:lines_header
+    len_header = len_header + strlength(headerlines(k)) +2;
 end
 
 from = find(startsWith(headerlines,'[')); % all header sections start with '['
@@ -23,9 +23,9 @@ to = [from(2:end)-1; lines_header]-1; % removes end paragraph break
 max_len=max(to-from,[],'all');
 header=string(zeros(length(from),max_len)); %create zeros string array of the size of the nb of sections * the length of the longest section
 
-for i = 1:length(from)
-    for j = 1:(to(i)-from(i))
-        header(i,j) = headerlines(from(i)+j-1);
+for k = 1:length(from)
+    for l = 1:(to(k)-from(k))
+        header(k,l) = headerlines(from(k)+l-1);
     end
 end
 
