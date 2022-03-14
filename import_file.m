bcfd803aebb52e0c32336e3a5d414031e23b5471
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-03-14 17:36:49 +0000

    Created file_info
    Added file_info function and corresponding section in import_file function

--- import_file.m
+++ import_file.m
@@ -3,7 +3,7 @@ function [outputArg1,outputArg2] = import_file(filename,imageNo)
 % This function imports the file, creates a formatted header and a frame
 % with all of the chosen image's data.
 
-% add file extension check
+% File extension check
 [~,~,ext] = fileparts(filename);
 if ext~='.nid'
     error('This file format is not supported. Please input a .nid file.')
@@ -13,28 +13,27 @@ end
 [len_header, header]=read_header(filename); %load header
 binlines = read_images(filename,header,len_header,imageNo); %load z data
 
+%Load essential info
+info=file_info(filename, header, imageNo);
+
 %Image info
 Dim0Min=get_image_info(header,imageNo,'Dim0Min',1);
 Dim1Min=get_image_info(header,imageNo,'Dim1Min',1);
-
 Dim0Range=get_image_info(header,imageNo,'Dim0Range',1);
 Dim1Range=get_image_info(header,imageNo,'Dim1Range',1);
 Dim2Range=get_image_info(header,imageNo,'Dim2Range',1);
-
-Dim0Length=get_image_info(header,imageNo,'Points',1);
-Dim1Length=get_image_info(header,imageNo,'Lines',1);
 Dim2Bits=get_image_info(header,imageNo,'SaveBits',1);
 
-%pre-allocate memory to frame
+%Pre-allocate memory to frame
 vartype={'double','double','double','double','double','double'};
 varname={'x','y','z','x_nm','y_nm','z_nm'};
 frame=table('Size',[length(binlines) 6],'VariableTypes',vartype,'VariableNames', varname); 
 
-%build frame
-x=repelem(1:Dim0Length,Dim0Length);
-x_nm=repelem(linspace(Dim0Min,Dim0Min+Dim0Range,Dim0Length),Dim0Length);
-y=repmat(1:Dim0Length,[1 Dim0Length]);
-y_nm=repmat(linspace(Dim1Min,Dim1Min+Dim1Range,Dim1Length),[1 Dim1Length]);
+%Build frame
+x=repelem(1:info('x.pixels'),info('x.pixels'));
+x_nm=repelem(linspace(Dim0Min,Dim0Min+Dim0Range,info('x.pixels')),info('x.pixels'));
+y=repmat(1:info('y.pixels'),[1 info('y.pixels')]);
+y_nm=repmat(linspace(Dim1Min,Dim1Min+Dim1Range,info('y.pixels')),[1 info('y.pixels')]);
 
 frame.x=x(:);
 frame.y=y(:);
@@ -43,7 +42,13 @@ frame.x_nm=x_nm(:)*1e9;
 frame.y_nm=y_nm(:)*1e9;
 frame.z_nm=binlines*(Dim2Range/(2^Dim2Bits))*1e9;
 
+%Add information to 'info' container
+info('x.conv')=(max(frame.x_nm))/(max(frame.x-1));
+info('y.conv')=(max(frame.y_nm))/(max(frame.y-1));
+info('z.conv')=frame.z_nm(1)/frame.z(1);
+
 outputArg1 = frame;
 outputArg2 = header;
+outputArg3 = info;
 end
 
