9e30125f47d782ee2f2b6092db7d5332f2fbe7bf
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-03-17 16:00:26 +0000

    Update GB_detection.m
    fixed mask frame and passing from seg_aspect_ratio

--- GB_detection.m
+++ GB_detection.m
@@ -8,11 +8,13 @@ segmentsthrough=[];
 av=median(C,"all");
 s=linspace(1,info('x.pixels'),gridsize);
 gb_detection=zeros(length(C));
+list_GBs={[0]};
+
 for k=1:(length(s)-1)
     for l=1:(length(s)-1)
         tot=tot+1;%total number of segments
         %figure(tot);
-        segment=C(s(l):s(l+1),s(k):s(k+1));
+        segment=C(s(l):(s(l+1)-1),s(k):(s(k+1)-1));
         %imagesc(segment);
         %colormap(gray);
         %c=colorbar;
@@ -23,24 +25,52 @@ for k=1:(length(s)-1)
         if seg_min<(av*avpct) %discard segment if its average is above the total median
             Mask=segment<(seg_min*minpct);
             
+            %frame
+            if tot==1
+                Mask(1:15,:)=0;
+                Mask(:,1:15)=0;
+
+            elseif tot==(gridsize-1)
+                Mask((end-15):end,:)=0;
+                Mask(:,1:15)=0;
+
+            elseif tot==((gridsize-1)^2-gridsize)
+                Mask(1:15,:)=0;
+                Mask(:,(end-15):end)=0;
+
+            elseif tot==(gridsize-1)^2
+                Mask((end-15):end,:)=0;
+                Mask(:,(end-15):end)=0;
+
+            end
+
             if mean(Mask,'all')==0
-                break
+                continue
             end
 
             if aspect_ratio==true
                 [pass pixels]=seg_aspect_ratio(Mask);
+                if pass==false
+                    pixels=[0 0; 0 0];
+                end
             else
                 [pass pixels]=seg_aspect_ratio(Mask);
                 pass=true;
             end
-
-            if pass==true % for segments that pass the checks:
+            
+            if (mean(pixels,'all')~=0 && pass~=false)==true % for segments that pass the checks
+                pixels(:,1)=pixels(:,1)+s(l);
+                pixels(:,2)=pixels(:,2)+s(k);
                 segmentsthrough(end+1)=tot;
-                gb_detection(s(l):s(l+1),s(k):s(k+1))= Mask;
+                gb_detection(s(l):(s(l+1)-1),s(k):(s(k+1)-1))= Mask;
+                list_GBs(end+1)={pixels};
             end
         end
     end
 end
+
+list_GBs=list_GBs(2:end);
+info('list_GBs')=list_GBs;
 
 %clear the edges with a frame of '0'
 gb_detection(1:15,:)=0;
@@ -59,7 +89,7 @@ title(sprintf('GB detection for avpct= %.3f and minpct= %.3f',avpct,minpct))
 set(gca,'fontsize',15);
 
 outputArg1 = gb_detection;
-outputArg2 = tot;
+outputArg2 = info;
 outputArg3 = segmentsthrough;
 end
 
