161db5a74e4f04a087db6b6d0c382f74fe473ead
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-03-18 15:46:17 +0000

    fix clear info

--- plot_GBs.m
+++ plot_GBs.m
@@ -3,9 +3,9 @@ function [outputArg1] = plot_GBs(info)
 %   Detailed explanation goes here
 
 list_GBs=info('list_GBs');
-if mean(contains(info.keys,('line_pixels')))~=0
-    remove(info,'line_pixels');
-    remove(info,'line_lengths');
+if mean(contains(info.keys,('GB_pixels')))~=0
+    remove(info,'GB_pixels');
+    remove(info,'GB_lengths');
 end
 
 for i=1:length(list_GBs)
@@ -34,6 +34,7 @@ laby=ylabel('y (pixels)');
 laby.FontSize = 16;
 axis square;
 title(sprintf('GB detection for avpct= 0.950 and minpct= 1.012'))
+set(gca,'fontsize',15);
 
 outputArg1 = info;
 
