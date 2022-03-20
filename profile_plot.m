3074594b2ecedf71f1afbebc409e3933fa087f6c
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-03-20 16:29:11 +0000

    add GB numbering for cumu prob coloring

--- profile_plot.m
+++ profile_plot.m
@@ -1,4 +1,4 @@
-function outputArg1=profile_plot(C,perp_line_pix,perp_line_len)
+function outputArg1=profile_plot(C,perp_line_pix,perp_line_len,lineno)
 %PROFILE_PLOT Plot line profile across a perp line item
 %   Detailed explanation goes here
 
@@ -9,7 +9,7 @@ for i=1:length(perp_line_pix)
 end
 
 %plot(perp_line_len,zData)
-geom_en=[0 0 0 0 0];
+geom_en=[0 0 0 0 0 0];
 
 %Global minimum
 i_min=find(zData==min(zData));
@@ -24,7 +24,7 @@ w_l=abs(perp_line_len(l_Imax(end))-perp_line_len_min); %half width of left profi
 beta_l=atand(4.6*d_l/(0.974*2*w_l)); %geom angle beta from Robertson
 halfdi_l=90-beta_l;  %geom angle half of psi (dihedral angle)
 en_ratio_l=2*cosd(halfdi_l); %energy ratio
-geom_en(1,:)=[(2*w_l) d_l beta_l (2*halfdi_l) en_ratio_l]; %combine all the info
+geom_en(1,:)=[(2*w_l) d_l beta_l (2*halfdi_l) en_ratio_l lineno]; %combine all the info
 
 %Right profile
 r_prof=zData(i_min:end);
@@ -35,7 +35,7 @@ w_r=abs(perp_line_len(r_Imax(1)+i_min(1)-1)-perp_line_len_min); %half width of r
 beta_r=atand(4.6*d_r/(0.974*2*w_r)); %geom angle beta from Robertson
 halfdi_r=90-beta_r;  %geom angle half of psi (dihedral angle)
 en_ratio_r=2*cosd(halfdi_r); %energy ratio
-geom_en(2,:)=[(2*w_r) d_r beta_r (2*halfdi_r) en_ratio_r]; %combine all the info
+geom_en(2,:)=[(2*w_r) d_r beta_r (2*halfdi_r) en_ratio_r lineno]; %combine all the info
 
 %Symmetric groove
 lr_max=(l_prof(l_Imax(end))+r_prof(r_Imax(1)))/2;
@@ -44,7 +44,7 @@ w_lr=abs(perp_line_len(l_Imax(end))-perp_line_len(r_Imax(1)+i_min(1)-1))/2; %hal
 beta_lr=atand(4.6*d_lr/(0.974*2*w_lr)); %geom angle beta from Robertson
 halfdi_lr=90-beta_lr;  %geom angle half of psi (dihedral angle)
 en_ratio_lr=2*cosd(halfdi_lr); %energy ratio
-geom_en(3,:)=[w_lr d_lr beta_lr (2*halfdi_lr) en_ratio_lr]; %combine all the info
+geom_en(3,:)=[w_lr d_lr beta_lr (2*halfdi_lr) en_ratio_lr lineno]; %combine all the info
 
 outputArg1=geom_en;
 end
