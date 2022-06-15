bf1b3d2b1d20f2ce330e6714c1fd5178ad07bac4
lamr18 <57528428+lamr18@users.noreply.github.com>
2022-06-15 12:27:59 +0100

    Frame fix

--- Script/GB_detection.m
+++ Script/GB_detection.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2,outputArg3] = GB_detection(info,C,gridsize,aspect_ratio,avpct,minpct)
+function [gb_detection,info,segmentsthrough] = GB_detection(info,C,gridsize,aspect_ratio,avpct,minpct)
 %GB_DETECTION Detects grain boundaries in the AFM image.
 % Please read more about choosing threshold parameters in the README file.
 
@@ -32,34 +32,27 @@ for k=1:(length(s)-1)
             %threshold of the minimum value of the segment. This makes sense for GBs as 
             %the GB is straight and should have a relatively stable value along the bottom of the groove.
             
-            Mask=segment<(seg_min*minpct);
+            Mask_s=segment<(seg_min*minpct);
            
             % Create a frame on the corner segments of the image, as these
             % segments will be hard to profile across. The corner segments
             % correspond to the 4 tot values below.
             if tot==1
-                Mask(1:20,:)=0;
-                Mask(:,1:20)=0;
-
-
-            elseif tot==(gridsize-1)
-                Mask((end-20):end,:)=0;
-                Mask(:,1:20)=0;
-     
-
-            elseif tot==(((gridsize-1)^2)-gridsize+2)
-                %Mask(1:20,:)=0;
-                %Mask(:,(end-20):end)=0;
-                Mask(:)=0;
-
-            elseif tot==(gridsize-1)^2
-                Mask((end-20):end,:)=0;
-                Mask(:,(end-20):end)=0;
-
+                Mask_s(1:20,:)=1;
+                Mask_s(:,1:20)=1;
+            elseif tot==(gridsize)
+                Mask_s((end-20):end,:)=1;
+                Mask_s(:,1:20)=1;
+            elseif tot==(((gridsize)^2)-gridsize+1)
+                Mask_s(1:20,:)=1;
+                Mask_s(:,(end-20):end)=1;
+            elseif tot==(gridsize)^2
+                Mask_s((end-20):end,:)=1;
+                Mask_s(:,(end-20):end)=1;
             end
             
             % If the mask is a matrix of zeros, segment can be skipped
-            if mean(Mask,'all')==0
+            if mean(Mask_s,'all')==0
                 continue
             end
             
@@ -70,12 +63,12 @@ for k=1:(length(s)-1)
             %determine shape. If aspect_ratio is 'false', then the shape
             %will be discarded in the detection.
             if aspect_ratio==true
-                [pass pixels]=seg_aspect_ratio(Mask);
+                [pass pixels]=seg_aspect_ratio(Mask_s);
                 if pass==false
                     pixels=[0 0; 0 0];
                 end
             else
-                [pass pixels]=seg_aspect_ratio(Mask);
+                [pass pixels]=seg_aspect_ratio(Mask_s);
                 pass=true;
             end
             
@@ -85,7 +78,7 @@ for k=1:(length(s)-1)
                 pixels(:,1)=pixels(:,1)+s(l);
                 pixels(:,2)=pixels(:,2)+s(k);
                
-                gb_detection(s(l):(s(l+1)),s(k):(s(k+1)))= Mask;
+                gb_detection(s(l):(s(l+1)),s(k):(s(k+1)))= Mask_s;
                 list_GBs(end+1)={pixels};
             end
         end
@@ -99,12 +92,6 @@ if mean(contains(info.keys,('list_GBs')))~=0
 end
 info('list_GBs')=list_GBs;
 
-% Clear the edges with a frame of zeros 
-gb_detection(1:20,:)=0;
-gb_detection((end-20):end,:)=0;
-gb_detection(:,1:20)=0;
-gb_detection(:,(end-20):end)=0;
-
 % This section allows to print only the GB_detection drawn by this function, 
 % rather than a combined plot from 'combined_plot.m'. Unhash to plot.
 %imshow(gb_detection)
@@ -117,8 +104,5 @@ gb_detection(:,(end-20):end)=0;
 %title(sprintf('GB detection for avpct= %.3f and minpct= %.3f',avpct,minpct))
 %set(gca,'fontsize',15);
 
-outputArg1 = gb_detection;
-outputArg2 = info;
-outputArg3 = segmentsthrough;
 end
 
